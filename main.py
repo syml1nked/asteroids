77ff25f0e3ee47538b645c4c3a385cde79c03733
syml1nked <rmoran38@gmail.com>
2025-09-14 11:21:58 -0700

    swap to sprite groups

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -14,10 +19,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updateable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
